466f269df00e07457983c254e441ce38b2cc956f
prabhtz <gunpaul41@gmail.com>
2025-03-06 12:42:54 -0500

    chore: update table column width

--- frontend/dwm-frontend/src/components/ThreatTable.tsx
+++ frontend/dwm-frontend/src/components/ThreatTable.tsx
@@ -9,31 +9,28 @@ interface ThreatTableProps {
 const ThreatTable: React.FC<ThreatTableProps> = ({ data }) => {
   return (
     <div className="overflow-x-auto rounded-lg border border-gray-700 shadow-lg">
-      <Table className="w-full table-fixed">
-
+      <Table className="w-full table-auto border-collapse">
         <TableHead>
           <TableRow className="bg-gray-800 text-white uppercase text-sm tracking-wider">
-            <TableCell className="p-4 text-lg font-bold w-[15%] min-w-[100px]">Source</TableCell>
-            <TableCell className="p-4 text-lg font-bold w-[50%] min-w-[300px]">Threat URL</TableCell>
-            <TableCell className="p-4 text-lg font-bold w-[35%] min-w-[200px]">Description</TableCell>
+            <TableCell className="p-4 text-lg font-bold w-1/6">Source</TableCell>
+            <TableCell className="p-4 text-lg font-bold w-3/6">Threat URL</TableCell>
+            <TableCell className="p-4 text-lg font-bold w-2/6">Description</TableCell>
           </TableRow>
         </TableHead>
-
 
         <TableBody>
           {data.map((item, index) => (
             <TableRow key={index} className="border-b border-gray-700">
-              <TableCell className="p-4 w-[15%] min-w-[100px]">{item.site}</TableCell>
+              <TableCell className="p-4 w-1/6">{item.site}</TableCell>
 
-
-              <TableCell className="p-4 w-[50%] min-w-[300px] break-words">
+              <TableCell className="p-4 w-3/6 break-words">
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <a
                       href={item.url}
                       target="_blank"
                       rel="noopener noreferrer"
-                      className="text-blue-400 break-words inline-block max-w-full truncate hover:underline"
+                      className="text-blue-400 break-words inline-block truncate hover:underline"
                     >
                       {item.url}
                     </a>
@@ -42,8 +39,7 @@ const ThreatTable: React.FC<ThreatTableProps> = ({ data }) => {
                 </Tooltip>
               </TableCell>
 
-
-              <TableCell className="p-4 w-[35%] min-w-[200px] break-words">
+              <TableCell className="p-4 w-2/6 break-words">
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <span className="block truncate hover:whitespace-normal">
