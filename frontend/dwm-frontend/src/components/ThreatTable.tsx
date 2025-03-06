860fb5fc7d864ef5f9d0a09093d39c58f4f69038
prabhtz <gunpaul41@gmail.com>
2025-03-06 12:47:20 -0500

    chore: update table column width

--- frontend/dwm-frontend/src/components/ThreatTable.tsx
+++ frontend/dwm-frontend/src/components/ThreatTable.tsx
@@ -9,21 +9,21 @@ interface ThreatTableProps {
 const ThreatTable: React.FC<ThreatTableProps> = ({ data }) => {
   return (
     <div className="overflow-x-auto rounded-lg border border-gray-700 shadow-lg">
-      <Table className="w-full table-auto border-collapse">
+      <Table className="w-full border-collapse">
         <TableHead>
           <TableRow className="bg-gray-800 text-white uppercase text-sm tracking-wider">
-            <TableCell className="p-4 text-lg font-bold w-1/6">Source</TableCell>
-            <TableCell className="p-4 text-lg font-bold w-3/6">Threat URL</TableCell>
-            <TableCell className="p-4 text-lg font-bold w-2/6">Description</TableCell>
+            <TableCell className="p-4 text-lg font-bold whitespace-nowrap w-[15%]">Source</TableCell>
+            <TableCell className="p-4 text-lg font-bold whitespace-nowrap w-[50%]">Threat URL</TableCell>
+            <TableCell className="p-4 text-lg font-bold whitespace-nowrap w-[35%]">Description</TableCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
           {data.map((item, index) => (
             <TableRow key={index} className="border-b border-gray-700">
-              <TableCell className="p-4 w-1/6">{item.site}</TableCell>
+              <TableCell className="p-4 w-[15%] whitespace-nowrap">{item.site}</TableCell>
 
-              <TableCell className="p-4 w-3/6 break-words">
+              <TableCell className="p-4 w-[50%] break-words">
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <a
@@ -39,7 +39,7 @@ const ThreatTable: React.FC<ThreatTableProps> = ({ data }) => {
                 </Tooltip>
               </TableCell>
 
-              <TableCell className="p-4 w-2/6 break-words">
+              <TableCell className="p-4 w-[35%] break-words">
                 <Tooltip>
                   <TooltipTrigger asChild>
                     <span className="block truncate hover:whitespace-normal">
